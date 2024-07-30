b7a639fa790257d6834e7a8354d28d2330053b7e
ArbinIndia-Nikhil <nikhil.kankanwadi@arbin.com>
2024-07-30 12:05:02 +0530

    changes to emp dashboard, added toast

--- src/components/EmpDashboard.tsx
+++ src/components/EmpDashboard.tsx
@@ -1,122 +1,142 @@
-import { PieChart } from '@mui/x-charts/PieChart';
-import dayjs from 'dayjs';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { PieChart } from "@mui/x-charts/PieChart";
+import dayjs from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function EmpDashboard(){
-    return(
-        <div>
+export default function EmpDashboard() {
+  const username = sessionStorage.getItem("username");
+  const leaves = () => {
+    toast.dark("Sent for approval", {
+      position: "top-center",
+      autoClose: 500,
+    });
+  };
 
-            {/* Header - Containd company name and Profile name for now */}
-            <div className=" flex justify-between bg-HRMSRed">
-                <p className=" text-HRMSBeige p-5 text-xl font-RobotoMono">Arbin Instruments</p>
-                <p className=" text-HRMSBeige p-5 text-xl font-RobotoMono">Profile Name</p>
-            </div>
+  const attendance = () => {
+    toast.dark("Attendance marked", {
+      position: "top-center",
+      autoClose: 500,
+    });
+  };
+  return (
+    <div className="">
+      {/* Header - Containd company name and Profile name for now */}
+      <div className=" flex justify-between bg-HRMSRed">
+        <p className=" text-HRMSBeige p-5 text-xl font-RobotoMono">
+          Arbin Instruments
+        </p>
+        <p className=" text-HRMSBeige p-5 text-xl font-RobotoMono">
+          {username}
+        </p>
+      </div>
 
-            {/* Body div 1 */}
-            <div className=" flex justify-between m-5 mt-10">
+      {/* Body div 1 */}
+      <div className=" flex justify-between m-5 mt-10">
+        {/* Left panel */}
 
-                {/* Left panel */}
-                <div className=' flex-1'>
-                    hello
-                </div>
-
-                {/* Right panel */}
-                <div className=' flex-col'>
-
-                    {/* Right Top */}
-                    <div className=' flex p-4 border border-HRMSBlack'>
-                    {/* Number of Emp */}
-                    <div className=' text-HRMSBlack font-RobotoMono'>
-                        <p className=' text-xl'>Total Employees</p>
-                        <p className=' text-8xl'>20</p>
-                    </div>
-
-                    {/* chart */}
-                    <PieChart
-                    series={[
-                        {
-                        data: [
-                            { id: 0, value: 8, label: 'software', color: '#73c1c0' },
-                            { id: 1, value: 4, label: 'sales', color:'#1c3b3a' },
-                            { id: 2, value: 3, label: 'service', color:'#82b7b5' },
-                            { id: 3, value: 3, label: 'production', color:'#31c3bd' },
-                            { id: 4, value: 1, label: 'logistics', color:'#1e3534' }
-                        ],
-                        },
-                    ]}
-                    width={600}
-                    height={300}
-                    />
-                    </div>
-
-                    {/* Right Bottom */}
-
-                    {/* Mark Attendance and Leave Application buttons */}
-
-                    <p className=' mt-14 mb-4 text-4xl'>Select Date</p>
-                    <div className='flex-1'>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DemoContainer
-                                components={[
-                                'DesktopDatePicker',
-                                ]}
-                            >
-                                <DemoItem label="From Date">
-                                    <DesktopDatePicker defaultValue={dayjs('2022-04-17')} />
-                                </DemoItem>
-                            </DemoContainer>
-                        </LocalizationProvider>
-                    </div>
-                    <button type="submit" className=" bg-HRMSBlack text-HRMSBeige text-xl p-3 rounded-md hover:bg-HRMSRed hover:text-HRMSBeige mt-4">Mark Attendance</button>
-
-
-                    <p className=' mt-14 mb-4 text-4xl'>Appy for Leaves</p>
-                   <div className='flex'>
-                    <Box sx={{ minWidth: 120 }}>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Nature</InputLabel>
-                            <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            // value={age}
-                            label="Nature Of Leaves"
-                            // onChange={handleChange}
-                            >
-                            <MenuItem value={10}>Personal Leave</MenuItem>
-                            <MenuItem value={20}>Sick Leave</MenuItem>
-                            <MenuItem value={30}>Half Day</MenuItem>
-                            <MenuItem value={30}>Comp Off</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Box>
-                    </div> 
-                    <div className='flex-1'>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DemoContainer
-                                components={[
-                                'DesktopDatePicker',
-                                ]}
-                            >
-                                <DemoItem label="From Date">
-                                    <DesktopDatePicker defaultValue={dayjs('2022-04-17')} />
-                                </DemoItem>
-                                <DemoItem label="To Date">
-                                    <DesktopDatePicker defaultValue={dayjs('2022-04-17')} />
-                                </DemoItem>
-                            </DemoContainer>
-                        </LocalizationProvider>
-                    </div>
-                    <button type="submit" className=" bg-HRMSBlack text-HRMSBeige text-xl p-3 rounded-md hover:bg-HRMSRed hover:text-HRMSBeige mt-4">APPLY</button>
-                </div>
-            </div>
+        <div className="flex-1">
+          <p className=" mt-14 mb-4 text-4xl">Select Date</p>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DesktopDatePicker"]}>
+              <DemoItem>
+                <DesktopDatePicker defaultValue={dayjs("2022-04-17")} />
+              </DemoItem>
+            </DemoContainer>
+          </LocalizationProvider>
+          <button
+            type="submit"
+            className=" bg-HRMSBlack text-HRMSBeige text-xl p-3 rounded-md hover:bg-HRMSRed hover:text-HRMSBeige mt-4"
+            onClick={attendance}
+          >
+            Mark Attendance
+          </button>
+          <ToastContainer />
         </div>
-    )
+
+        {/* Right panel */}
+        <div className=" flex-col">
+          {/* Right Top */}
+          <div className=" flex p-4 border border-HRMSBlack">
+            {/* Number of Emp */}
+            <div className=" text-HRMSBlack font-RobotoMono">
+              <p className=" text-xl">Total Employees</p>
+              <p className=" text-8xl">20</p>
+            </div>
+
+            {/* chart */}
+            <PieChart
+              series={[
+                {
+                  data: [
+                    { id: 0, value: 9, label: "software", color: "#73c1c0" },
+                    { id: 1, value: 4, label: "sales", color: "#1c3b3a" },
+                    { id: 2, value: 3, label: "service", color: "#82b7b5" },
+                    { id: 3, value: 2, label: "production", color: "#31c3bd" },
+                    { id: 4, value: 1, label: "logistics", color: "#1e3534" },
+                    { id: 5, value: 2, label: "management", color: "#1e3550" },
+                  ],
+                },
+              ]}
+              width={600}
+              height={300}
+            />
+          </div>
+
+          {/* Right Bottom */}
+
+          {/* Mark Attendance and Leave Application buttons */}
+
+          <p className=" mt-14 mb-4 text-4xl">Appy for Leaves</p>
+          <div className="flex">
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Nature</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  // value={age}
+                  label="Nature Of Leaves"
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={10}>Personal Leave</MenuItem>
+                  <MenuItem value={20}>Sick Leave</MenuItem>
+                  <MenuItem value={30}>Half Day</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+          <div className="flex-1">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={["DesktopDatePicker"]}>
+                <DemoItem label="From Date">
+                  <DesktopDatePicker defaultValue={dayjs("2022-04-17")} />
+                </DemoItem>
+                <DemoItem label="To Date">
+                  <DesktopDatePicker defaultValue={dayjs("2022-04-17")} />
+                </DemoItem>
+              </DemoContainer>
+            </LocalizationProvider>
+          </div>
+          <button
+            type="submit"
+            className=" bg-HRMSBlack text-HRMSBeige text-xl p-3 rounded-md hover:bg-HRMSRed hover:text-HRMSBeige mt-4"
+            onClick={leaves}
+          >
+            APPLY
+          </button>
+          <ToastContainer />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,23 +1,65 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Login(){
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    return(
-        <div className=" h-screen bg-HRMSRed">
-            <div className=" flex justify-between">
-                <p className=" text-HRMSBeige p-5 text-3xl font-RobotoMono">Arbin <br />Instruments</p>
-                <p className=" text-HRMSBeige p-5 text-3xl font-RobotoMono">HRMS</p>
-            </div>
-            <div className=" flex flex-col justify-center items-center text-HRMSBeige">
-                <p className=" font-Bungee text-HRMSBeige text-9xl m-9 p-10">WELCOME</p>
-                <input name="email id" placeholder="email id" className=" placeholder:text-gray-500 placeholder:text-xl placeholder:p-1 pb-1 text-HRMSBlack bg-HRMSBeige m-4 text-2xl rounded-md"/>
-                <input name="password" placeholder="password" className=" placeholder:text-gray-500 placeholder:text-xl placeholder:p-1 pb-1 text-HRMSBlack bg-HRMSBeige m-4 text-2xl rounded-md"/>
-                <button type="submit" className=" bg-HRMSBeige text-HRMSBlack text-2xl p-2 rounded-md hover:bg-HRMSBlack hover:text-HRMSBeige">LOGIN</button>
-            </div>
-            <div className=" w-[18%] h-[18%]">
-                <img src="../src/assets/loginimg.png" />
-            </div>
-        </div>
-    )
+  const handleLogin = () => {
+    axios
+      .post("https://localhost:7289/api/Auth/login", { username, password })
+      .then((res) => {
+        const { token } = res.data;
+        sessionStorage.setItem("username", username);
+        if (res.status === 200) {
+          navigate("/employee-dashboard");
+        }
+      })
+      .catch((err) => {
+        console.error("Login failed", err);
+      });
+  };
+
+  return (
+    <div className=" h-screen bg-HRMSRed">
+      <div className=" flex justify-between">
+        <p className=" text-HRMSBeige p-5 text-3xl font-RobotoMono">
+          Arbin <br />
+          Instruments
+        </p>
+        <p className=" text-HRMSBeige p-5 text-3xl font-RobotoMono">HRMS</p>
+      </div>
+      <div className=" flex flex-col justify-center items-center text-HRMSBeige">
+        <p className=" font-Bungee text-HRMSBeige text-9xl m-9 p-10">WELCOME</p>
+        <input
+          name="email id"
+          placeholder="email id"
+          className=" placeholder:text-gray-500 placeholder:text-xl placeholder:p-1 pb-1 text-HRMSBlack bg-HRMSBeige m-4 text-2xl rounded-md"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          name="password"
+          placeholder="password"
+          className=" placeholder:text-gray-500 placeholder:text-xl placeholder:p-1 pb-1 text-HRMSBlack bg-HRMSBeige m-4 text-2xl rounded-md"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className=" bg-HRMSBeige text-HRMSBlack text-2xl p-2 rounded-md hover:bg-HRMSBlack hover:text-HRMSBeige"
+          onClick={handleLogin}
+        >
+          LOGIN
+        </button>
+      </div>
+      <div className=" w-[18%] h-[18%]">
+        <img src="../src/assets/loginimg.png" />
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
